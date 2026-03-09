ad6bbbc94fbedfe02517303f2b04ab09a580630c
Ananthakrishnan <ananthmle@gmail.com>
2026-03-09 23:14:42 +0530

    Fix cleanup code

--- backend/llm_config.py
+++ backend/llm_config.py
@@ -1,12 +1,11 @@
-from langchain_openai import ChatOpenAI
+import os
 import streamlit as st
-# from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
 
+# Load API key from Streamlit secrets
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
-# load_dotenv()
-
-llm=ChatOpenAI(
+llm = ChatOpenAI(
     model="gpt-4o-mini",
-    temperature=0.2,
-    openai_api_key=st.secrets["OPENAI_API_KEY"]
+    temperature=0.2
 )
